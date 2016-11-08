47509638811ac4c0fde10d29e79ea85eb12cd78e
Wattanit Hotrakool <wattanit.h@gmail.com>
2016-11-08 23:31:43 +0000

    Add serial connection class

--- libPiLite.py
+++ libPiLite.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 
+import serial
+import time, sys
+
+class PiLiteSerial:
+    def __init__(self):
+        self.s = serial.Serial()
+        self.s.baudrate = 9600
+        self.s.timeout = 0
+        self.s.port = "/dev/serial0"
+        self.open()
+        self.write("$$$ALL,OFF\r")
+
+    def open(self):
+        try:
+            self.s.open()
+        except serial.SerialException, e:
+            sys.stderr.write("Could not open port %r: %s\n" % (self.s.port, e))
+            sys.exit(1)
+
+    def write(self, msg):
+        self.s.write(msg)
+
+    def writeBinary(self, msg):
+        msg = "$$$F"+msg+"\r"
+        self.s.write(msg)
+
 def createBlankGrid(row,column):
     blankgrid = [[0 for x in range(column)] for y in range(row)]
     
